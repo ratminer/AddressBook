80b43e9ffe433405ab7cb759628cf61223e471ad
robertfernandes <ratminer_54321@hotmail.com>
2017-02-09 11:22:27 -0500

    updating travis.yml

--- src/main/java/webApp/AddressBook.java
+++ src/main/java/webApp/AddressBook.java
@@ -1,17 +1,34 @@
 package webApp;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 /**
  * Created by robertfernandes on 2/9/2017.
  */
-@Data
 public class AddressBook {
 
+    @Getter@Setter
     private int addressBookId;
+    @Getter@Setter
     private List<BuddyInfo> buddyInfoList;
 
+    public void addBuddy(BuddyInfo buddyInfo) {
+        buddyInfoList.add(buddyInfo);
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        for (BuddyInfo bi : buddyInfoList) {
+            sb.append(bi.toString());
+            sb.append("/n");
+        }
+
+        return sb.toString();
+    }
 
 }
---
+++ src/main/java/webApp/AddressBookRepository.java
@@ -0,0 +1,8 @@
+package webApp;
+
+/**
+ * Created by robertfernandes on 2/9/2017.
+ */
+
+public class AddressBookRepository {
+}

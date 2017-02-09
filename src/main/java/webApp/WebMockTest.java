b9c18cf680504aad6a693ed2aac2ea089f9cd871
robertfernandes <ratminer_54321@hotmail.com>
2017-02-09 10:41:51 -0500

    initial commit

--- src/main/java/webApp/WebMockTest.java
+++ src/main/java/webApp/WebMockTest.java
@@ -14,8 +14,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import webApp.GreetingController;
-import webApp.GreetingService;
 
 /**
  * Created by robertfernandes on 2/9/2017.
